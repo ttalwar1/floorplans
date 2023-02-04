d085f41c3767a264ee01c37775fafbcd58ccdb6a
tony.talwar@gmail.com <tony.talwar@gmail.com>
2023-02-03 23:41:45 -0800

    updated scullyfloorplans in environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,10 @@ export const environment = {
     API: {
         BASE_URL: 'https://hhcwebapi.azurewebsites.net/api',
     },
-    fp: 'floorplans'
+    fp: 'scullyfloorplans'
 };
+
+
+//environment variable fp is used to toggle which API I call
+//api with floorplans end point returns spaces
+//api with scullyfloorplans replaces spaces with '-'
